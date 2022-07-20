7879276e46d665eb103509b90f0b8a49037371e5
ngotokodawari <naoya.goto@kodawari-japan.co.jp>
2022-07-20 17:18:25 +0900

    propsで引数を渡す

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
-import  React,{useState} from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <LikeButton />
+        <LikeButton value = '1'/>
       </header>
     </div>
-    
   );
 }
 
-function LikeButton(){
-  const [count,setCount] = useState(999); // count=999
+// 関数コンポーネント
+function LikeButton(props) {
+  const [count, setCount] = useState(props.value); // count=999
   const handleClick = () => {
-    setCount(count+1);
-  }
-  return <span className="likeButton" onClick={handleClick}>いいぞ{count}</span>
+    setCount(parseInt(count) + 1);
+  };
+  return (
+    <span className="likeButton" onClick={handleClick}>
+      いいぞ{count}
+    </span>
+  );
 }
--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
+
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
